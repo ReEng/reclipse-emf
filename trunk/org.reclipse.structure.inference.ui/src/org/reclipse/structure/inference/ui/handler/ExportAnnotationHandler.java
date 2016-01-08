1bc015535918b368d41a81d749d88e88bec1e1a6
lehrig <sebastian.lehrig@informatik.tu-chemnitz.de>
2016-01-08 11:37:58 +0100

    Now iterating over the selection and printing the detected pattern name

--- trunk/org.reclipse.structure.inference.ui/src/org/reclipse/structure/inference/ui/handler/ExportAnnotationHandler.java
+++ trunk/org.reclipse.structure.inference.ui/src/org/reclipse/structure/inference/ui/handler/ExportAnnotationHandler.java
@@ -1,22 +1,33 @@
 package org.reclipse.structure.inference.ui.handler;
 
+import java.util.Iterator;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.TreeSelection;
 import org.eclipse.ui.handlers.HandlerUtil;
-import org.reclipse.structure.inference.ui.views.annotations.AnnotationView;
+import org.reclipse.structure.inference.annotations.ASGAnnotation;
 
 public class ExportAnnotationHandler extends AbstractHandler {
 
     @Override
     public Object execute(final ExecutionEvent event) throws ExecutionException {
-        final IWorkbenchPart part = HandlerUtil.getActivePart(event);
+        final ISelection selection = HandlerUtil.getCurrentSelectionChecked(event);
 
-        if (part instanceof AnnotationView) {
-            final AnnotationView view = (AnnotationView) part;
+        if (selection instanceof TreeSelection) {
+            final TreeSelection treeSelection = (TreeSelection) selection;
 
-            System.out.println("LEHRIG: ExportAnnotationHandler called for view \"" + view.getTitle() + "\"");
+            for (final Iterator<?> iter = treeSelection.iterator(); iter.hasNext();) {
+                final Object node = iter.next();
+
+                if (node instanceof ASGAnnotation) {
+                    final ASGAnnotation annotation = (ASGAnnotation) node;
+                    System.out.println("LEHRIG: ExportAnnotationHandler called for annotation \""
+                            + annotation.getPattern().getName() + "\"");
+                }
+            }
         }
 
         return null;
